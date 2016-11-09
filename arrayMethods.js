26a72f40673304401fe3333885b2787620e098b3
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-11-08 19:04:40 -1000

    finished

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,99 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+//PUSH()
+
+var nameArray = [];
+
+nameArray.push('Chaz', 'Castro');
+
+//
+
+for ( var i = 6; i <= 10; i ++ ) {
+  genericNumberArray.push(i);
+}
+
+//POP()
+
+var popResult = colors.pop();
+
+//REVERSE()
+
+scrambledWords.reverse();
+
+phoneNumber.reverse();
+
+//SHIFT()
+
+var nextOrder = orderQueue.shift();
+
+//SORT()
+
+mixedNums.sort();
+
+mixedWords.sort();
+
+//SPLICE()
+
+var notFruit = fruitCollection.splice(1, 3);
+
+gemBox.splice(5, 7, 'Sapphire');
+
+upToTen.splice(2, 0, 3, 4, 5, 6, 7, 8);
+
+brownOnly.splice(1, 5, 'brown', 'brown', 'brown');
+
+//UNSHIFT()
+
+var orderedValuesNewLength = orderedValues.unshift(1, 2, 3);
+
+//CONCAT()
+
+var randomThingsArray = genericNumberArray.concat(colors);
+
+var updatedOrders = orderQueue.concat({ takeOut: 'Sushi' }, { takeOut: 'Coffee' });
+
+//JOIN()
+
+var sentence = scrambledWords.join(' ');
+
+var myFruits = fruitCollection.join(' + ');
+
+//SLICE()
+
+favoriteFriends = friends.slice(1, 3);
+
+var owesMoney = friends.slice(3);
+
+//TOSTRING()
+
+var monthNameString = monthNames.toString();
+
+//INDEXOF()
+
+var favoriteColor = colors.indexOf('Red');
+
+var favoriteEvenNumber = mixedNums.indexOf(76);
+
+//LASTINDEXOF()
+
+var lastNine = bulkNumbers.lastIndexOf(9);
+
+var lastFive = bulkNumbers.lastIndexOf(5);
+
+/*var genericNumberArray = [1, 2, 3, 4, 5];
+var colors = ['Blue', 'Red', 'Yellow', 'Charlie'];
+var scrambledWords = ['win', 'the', 'for', 'burritos'];
+var phoneNumber = [9, 0, 3, 5, 7, 6, 8];
+var orderQueue = [{ takeOut: 'Ice Cream' }, { takeOut: 'Medium Salad'}, { takeOut: 'Burger'}];
+var mixedNums = [5, 81, 40, 4, 805, 10, 76, 21, 3, 2, 7, 1, 7];
+var mixedWords = ['Dont', 'You', 'Forget', 'About', 'Me'];
+var fruitCollection = ['Apple', 'Scissors', 'Pug', 'Lady Bug', 'Banana', 'Pear'];
+var gemBox = ['Ruby', 'Diamond', 'Diamond', 'Emerald', 'Moonstone', 'Pizza', 'Sneaker', 'Sapphire', 'Baby', 'Jeans', 'Soda Can', 'Boat'];
+var upToTen = [1, 2, 9, 10];
+var brownOnly = ['brown', 'brownish', 'ad8fy7iuhj23nrsf', 9813, {}, undefined,  'brown'];
+var orderedValues = [4, 5, 6, 7, 8, 9, 10];
+var friends = ['Todd', 'Jacoby', 'Miko', 'Joseph', 'Kevin', 'Todd C.'];
+var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
+var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];*/
